3536a21c166642a3de331b980ea71e7af8c27111
s.haertlein <steffen.haertlein@gmx.de>
2015-11-28 21:22:40 +0100

    navigation automation beginnings

--- templates/membernav.php
+++ templates/membernav.php
@@ -1,9 +1,9 @@
 <ul class="nav nav-tabs nav-justified">
-	<li class="active" role="presentation">
+	<li id="members" class="active" role="presentation">
 		<a href="?v=members">Lehrgänge</a>
 	</li>
-	<li class="dropdown" role="presentation"> 
-		<a role="button" href="" data-toggle="dropdown" class="dropdown-toggle"> 
+	<li id="settings" class="dropdown" role="presentation"> 
+		<a role="button" data-toggle="dropdown" class="dropdown-toggle"> 
 			Einstellungen <span class="caret"></span> 
 		</a> 
 		<ul class="dropdown-menu"> 
@@ -16,3 +16,14 @@
 		</ul> 
 	</li>
 </ul>
+
+<!-- Aktivsetzung anhand aktueller Seite setzen (Controller setzt den Wert, ) -->
+<script type="text/javascript">
+	var Idactive = "<?php echo $this->_['active']; ?>"; // TODO: Variable setzen
+	var elem = document.getElementById(Idactive);
+	var parent = elem.parentNode;
+	for (var i = parent.childNodes.length - 1; i >= 0; i--) {
+		parent.childNodes[i].removeClass('active');
+	};
+	elem.addClass('active');
+</script>
